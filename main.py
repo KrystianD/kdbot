0339856b482bceb681589f182121a8bfc2f04c2a
Krystian Dużyński <krystekd@gmail.com>
2014-02-03 23:50:52 +0100

    added password when creating object

--- main.py
+++ main.py
@@ -212,7 +212,7 @@ class CLI(threading.Thread):
 
 random.seed()
 
-client = IRCBot("sendak.freenode.net", 6667, irc_nick, irc_channel)
+client = IRCBot("sendak.freenode.net", 6667, irc_nick, irc_channel, irc_pass)
 ext.pm = client.pluginManager
 client.reloadPlugins()
 
